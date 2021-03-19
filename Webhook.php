a64f1ea87ff50e5a235459a5afc2c3fbc662fad6
wolf-wetal <52989571+wolf-wetal@users.noreply.github.com>
2021-03-19 15:40:02 +0600

    Вывод в чат информацию
    Вывод в чат информацию

--- Webhook.php
+++ Webhook.php
@@ -3,21 +3,23 @@
 include_once ('Telegram.php');
 include_once ('ApiLeads.php');
 
-$body = file_get_contents('php://input'); //Получаем в $body json строку
-$arr = json_decode($body, true); //Разбираем json запрос на массив в переменную $arr
+$body = file_get_contents('php://input');
+$arr = json_decode($body, true);
 
-function cir_strrev($stroka){ //Так как функция strrev не умеет нормально переворачивать кириллицу, нужен костыль через массив. Создадим функцию
-    preg_match_all('/./us', $stroka, $array);
-    return implode('',array_reverse($array[0]));
-}
 $telegram = new Telegram('https://api.telegram.org/bot', '1773836592:AAFCTJi5gtiJUi_0zrWpgOJ-P7jR5p2xDFA', '-1001498859797');
 $leads = new ApiLeads('http://api.leads.su/webmaster','ecf17c85e3179028e287eb511016b1fc');
 
-//$getTelegram = $telegram->setRequest('test');
+if (trim($arr['message']['text']) == '/getCountry') {
+    $getCountry = $leads->getCountries(50, 0, true, 10);
+    foreach ($getCountry as $country) {
+        $telegram->setRequest($country['id']);
+        $telegram->setRequest($country['name']);
+    }
 
-//$getCountry = $leads->getCountries(50, 0, true, 10);
+}
+if (trim($arr['message']['text']) == '/getAccount') {
+    $getIdName = $leads->getIdName();
+    $telegram->setRequest($getIdName['id']);
+    $telegram->setRequest($getIdName['name']);
 
-
-$getIdName = $leads->getIdName();
-
-    var_dump($getIdName);die();
+}
